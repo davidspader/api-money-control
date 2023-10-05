a5a2853d47fa9c53fa039df0f33cf10b5e1bcbb1
David Ambrozio <davidambroziodev@gmail.com>
2023-10-05 15:19:21 -0300

    create database and users table

---
+++ app/database/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column('id', Integer, primary_key=True, nullable=False, autoincrement=True)
+    username = Column('username', String, nullable=False, unique=True)
+    password = Column('password', String, nullable=False)
