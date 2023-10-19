0a393ad4fa4d0cb4639853622ce594821660939a
David Ambrozio <davidambroziodev@gmail.com>
2023-10-18 21:59:00 -0300

    fix docker and migrations

--- app/db/connection.py
+++ app/db/connection.py
@@ -2,7 +2,7 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = config('DATABASE_URL')
+DB_URL = config('DB_URL')
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DB_URL, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
