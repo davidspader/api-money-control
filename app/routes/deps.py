d40363c03772bb020e6e5c16ee931f15d72e4d44
David Ambrozio <davidambroziodev@gmail.com>
2023-10-20 11:54:56 -0300

    add auth to deps and update .env

--- app/routes/deps.py
+++ app/routes/deps.py
@@ -1,4 +1,12 @@
+from fastapi import Depends
+from decouple import config
+from sqlalchemy.orm import Session as SessionSqlAlchemy
+from fastapi.security import OAuth2PasswordBearer
 from app.db.connection import Session
+from app.use_cases.user import UserUseCases
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/user/login')
+TEST_MODE = config('TEST_MODE', default=False, cast=bool)
 
 def get_db_session():
     try:
@@ -6,3 +14,13 @@ def get_db_session():
         yield session
     finally:
         session.close()
+
+def auth(
+    db_session: SessionSqlAlchemy = Depends(get_db_session),
+    token = Depends(oauth_scheme)
+):
+    if TEST_MODE:
+        return
+    
+    uc = UserUseCases(db_session=db_session)
+    uc.verify_token(token=token)
