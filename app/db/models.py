9d7154acfdccde0624843231ac1fe7d29b5209db
David Ambrozio <davidambroziodev@gmail.com>
2023-10-21 21:52:31 -0300

    create categories table migrations

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db.base import Base
 
 class User(Base):
@@ -6,3 +6,9 @@ class User(Base):
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     username = Column('username', String, nullable=False, unique=True)
     password = Column('password', String, nullable=False)
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String, nullable=False)
+    user_id = Column('user_id', ForeignKey('users.id'), nullable=False)
