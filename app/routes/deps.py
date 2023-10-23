4383d2b07cc94349beaa5e7a10521d5d41eeca7d
David Ambrozio <davidambroziodev@gmail.com>
2023-10-23 16:15:00 -0300

    remove testmode from deps

--- app/routes/deps.py
+++ app/routes/deps.py
@@ -1,12 +1,10 @@
 from fastapi import Depends
-from decouple import config
 from sqlalchemy.orm import Session as SessionSqlAlchemy
 from fastapi.security import OAuth2PasswordBearer
 from app.db.connection import Session
 from app.use_cases.user import UserUseCases
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl='/user/login')
-TEST_MODE = config('TEST_MODE', default=False, cast=bool)
 
 def get_db_session():
     try:
