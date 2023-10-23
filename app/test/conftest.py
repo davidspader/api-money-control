eed35f4799083a312edb766242ddd7fb9fb64486
David Ambrozio <davidambroziodev@gmail.com>
2023-10-23 15:43:48 -0300

    create authentication_token fixture

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,8 +1,14 @@
 import pytest
+from jose import jwt
+from decouple import config
 from app.db.connection import Session
 from passlib.context import CryptContext
 from app.db.models import User as UserModel
+from app.use_cases.user import UserUseCases
+from app.schemas.user import User
 
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
 cryptContext = CryptContext(schemes=['sha256_crypt'])
 
 @pytest.fixture()
@@ -28,3 +34,17 @@ def user_on_db(db_session):
 
     db_session.delete(user)
     db_session.commit()
+
+@pytest.fixture()
+def authentication_token(db_session, user_on_db):
+    user = User(
+        username=user_on_db.username,
+        password='pass#'
+    )
+
+    uc = UserUseCases(db_session=db_session)
+    token_data = uc.user_login(user=user, expires_in=1)
+
+    headers = {"Authorization": f"Bearer {token_data.access_token}"}
+
+    yield headers
