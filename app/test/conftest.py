047028fca1663be5ac78e62528df0f48874aff32
David Ambrozio <davidambroziodev@gmail.com>
2023-10-24 22:13:51 -0300

    add categories_on_db fixture

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -3,6 +3,7 @@ from decouple import config
 from app.db.connection import Session
 from passlib.context import CryptContext
 from app.db.models import User as UserModel
+from app.db.models import Category as CategoryModel
 from app.use_cases.user import UserUseCases
 from app.schemas.user import User
 
@@ -63,3 +64,43 @@ def authentication_token(db_session, user_on_db):
     headers = {"Authorization": f"Bearer {token_data.access_token}"}
 
     yield headers
+
+@pytest.fixture()
+def authentication_token(db_session, user_on_db):
+    user = User(
+        username=user_on_db.username,
+        password='pass#'
+    )
+
+    uc = UserUseCases(db_session=db_session)
+    token_data = uc.user_login(user=user, expires_in=1)
+
+    headers = {"Authorization": f"Bearer {token_data.access_token}"}
+
+    yield headers
+
+@pytest.fixture()
+def categories_on_db(db_session, user_on_db):
+    data = [user_on_db.id]
+    
+    categories = [
+        CategoryModel(name='category 1', user_id=user_on_db.id),
+        CategoryModel(name='category 2', user_id=user_on_db.id),
+        CategoryModel(name='category 3', user_id=user_on_db.id),
+        CategoryModel(name='category 4', user_id=user_on_db.id)
+    ]
+
+    for category in categories:
+        db_session.add(category)
+    db_session.commit()
+
+    for category in categories:
+        db_session.refresh(category)
+    
+    data.append(categories)
+    
+    yield data
+
+    for category in categories:
+        db_session.delete(category)
+    db_session.commit()
