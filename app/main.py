455de9816b8a179739fb01c121e7c610838a7fc2
David Ambrozio <davidambroziodev@gmail.com>
2023-10-31 10:07:34 -0300

    add expense router to main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
 from app.routes.category_routes import router as category_router
+from app.routes.expense_routes import router as expense_router
 
 app = FastAPI()
 
@@ -10,3 +11,4 @@ def health_check():
 
 app.include_router(user_router)
 app.include_router(category_router)
+app.include_router(expense_router)
