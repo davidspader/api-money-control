7f98f7cf4314aece997ddcffd1e5ac3fca4f6dd7
David Ambrozio <davidambroziodev@gmail.com>
2023-10-31 08:37:50 -0300

    remove user_on_db from category schemas tests

--- app/test/schemas/test_category_schema.py
+++ app/test/schemas/test_category_schema.py
@@ -1,22 +1,22 @@
 import pytest
 from app.schemas.category import Category, CategoryOutput, CategoryInput
 
-def test_category_schema(user_on_db):
+def test_category_schema():
     category=Category(
         name='category name',
-        user_id=user_on_db.id
+        user_id=1
     )
 
     assert category.dict() == {
         'name': 'category name',
-        'user_id': user_on_db.id
+        'user_id': 1
     }
 
-def test_category_schema_invalid_name(user_on_db):
+def test_category_schema_invalid_name():
     with pytest.raises(ValueError):
         Category(
             name='',
-            user_id=user_on_db.id
+            user_id=1
         )
 
 def test_category_schema_invalid_user_id():
@@ -26,24 +26,24 @@ def test_category_schema_invalid_user_id():
             user_id='invalid'
         )
 
-def test_category_schema_output(user_on_db):
+def test_category_schema_output():
     categoryOutput = CategoryOutput(
         name='category name',
-        user_id=user_on_db.id,
+        user_id=1,
         id=1
     )
 
     assert categoryOutput.dict() == {
         'name': 'category name',
-        'user_id': user_on_db.id,
+        'user_id': 1,
         'id': 1
     }
 
-def test_category_schema_output_invalid_id(user_on_db):
+def test_category_schema_output_invalid_id():
     with pytest.raises(ValueError):
         CategoryOutput(
             name='category name',
-            user_id=user_on_db.id,
+            user_id=1,
             id='invalid'
         )
 
@@ -56,7 +56,7 @@ def test_category_schema_input():
         'name': 'category name',
     }
 
-def test_category_schema_input_invalid(user_on_db):
+def test_category_schema_input_invalid():
     with pytest.raises(ValueError):
         CategoryInput(
             name='',
