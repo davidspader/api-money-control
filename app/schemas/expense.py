5b9e249b4502cb84168169445a4bdcc634efcee7
David Ambrozio <davidambroziodev@gmail.com>
2023-10-31 08:04:06 -0300

    create validate to expense schemas and tests

--- app/schemas/expense.py
+++ app/schemas/expense.py
@@ -5,6 +5,19 @@ class Expense(CustomBaseModel):
     description: str
     value: float
 
+    @field_validator('description')
+    def validate_name(cls, value):
+        if len(value) == 0:
+            raise ValueError("empty description not allowed")
+        return value
+    
+    @field_validator('value')
+    def validate_value(cls, value):
+        if value <= 0:
+            raise ValueError("Invalid value")
+        return value
+
 class ExpenseInput(CustomBaseModel):
     category_id: int
     expense: Expense
+    
--- app/test/schemas/test_expense_schema.py
+++ app/test/schemas/test_expense_schema.py
@@ -1,31 +1,52 @@
+import pytest
 from app.schemas.expense import Expense, ExpenseInput
 
-def test_expense_schema():
-   expense = Expense(
-        description = 'Expense description',
-        value = 99.99,
-   )
 
-   assert expense.dict() == {
-        'description': 'Expense description',
-        'value': 99.99,
-   }
+def test_expense_schema():
+    expense = Expense(
+        description="Expense description",
+        value=99.99,
+    )
+
+    assert expense.dict() == {
+        "description": "Expense description",
+        "value": 99.99,
+    }
+
+
+def test_expense_schema_invalid_value():
+    with pytest.raises(ValueError):
+        Expense(
+            description="Expense description",
+            value="invalid",
+        )
+
+    with pytest.raises(ValueError):
+        Expense(
+            description="Expense description",
+            value=0,
+        )
+
+def test_expense_schema_invalid_description():
+    with pytest.raises(ValueError):
+        Expense(
+            description="",
+            value=99.99,
+        )
+
 
 def test_expense_input_schema():
-   expense = Expense(
-        description = 'Expense description',
-        value = 99.99,
-   )
+    expense = Expense(
+        description="Expense description",
+        value=99.99,
+    )
 
-   expense_input = ExpenseInput(
-      category_id = 1,
-      expense=expense
-   )
+    expense_input = ExpenseInput(category_id=1, expense=expense)
 
-   assert expense_input.dict() == {
-        'category_id': 1,
-        'expense': {
-            'description': 'Expense description',
-            'value': 99.99,
-        }
-   }
+    assert expense_input.dict() == {
+        "category_id": 1,
+        "expense": {
+            "description": "Expense description",
+            "value": 99.99,
+        },
+    }
