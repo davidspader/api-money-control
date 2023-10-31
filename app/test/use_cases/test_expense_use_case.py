435bae75cc3745607fd8b50a3d54ee27d8e52e09
David Ambrozio <davidambroziodev@gmail.com>
2023-10-31 09:43:54 -0300

    create add expense use case and test

---
+++ app/test/use_cases/test_expense_use_case.py
@@ -0,0 +1,25 @@
+from app.db.models import Expense as ExpenseModel
+from app.schemas.expense import Expense
+from app.use_cases.expense import ExpenseUseCases
+
+def test_add_expense_uc(db_session, category_on_db):
+    category = category_on_db[2]
+
+    uc = ExpenseUseCases(db_session=db_session)
+
+    expense = Expense(
+        description='Expense description',
+        value=99.99
+    )
+
+    uc.add_expense(expense=expense, category_id=category.id)
+
+    expense_on_db = db_session.query(ExpenseModel).first()
+
+    assert expense_on_db is not None
+    assert expense_on_db.description == expense.description
+    assert expense_on_db.value == expense.value
+    assert expense_on_db.category.name == category.name
+
+    db_session.delete(expense_on_db)
+    db_session.commit()
---
+++ app/use_cases/expense.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from fastapi.exceptions import HTTPException
+from fastapi import status
+from app.db.models import Expense as ExpenseModel
+from app.db.models import Category as CategoryModel
+from app.schemas.expense import Expense
+
+class ExpenseUseCases:
+    def __init__(self, db_session: Session):
+        self.db_session = db_session
+    
+    def add_expense(self, expense: Expense, category_id: int):
+        category = self.db_session.query(CategoryModel).filter_by(id=category_id).first()
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Category not found'
+            )
+        
+        expense_model = ExpenseModel(**expense.dict())
+        expense_model.category_id = category_id
+
+        self.db_session.add(expense_model)
+        self.db_session.commit()
