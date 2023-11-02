6279781671a9d1055af084fd1a7fe3c460776ee5
David Ambrozio <davidambroziodev@gmail.com>
2023-11-01 22:33:02 -0300

    fix tabs on test_expense_routes

--- app/test/routes/test_expense_routes.py
+++ app/test/routes/test_expense_routes.py
@@ -5,29 +5,30 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_add_expense_route(db_session, category_on_db):
-  token = category_on_db[0]
-  user = category_on_db[1]
-  category = category_on_db[2]
+    token = category_on_db[0]
+    user = category_on_db[1]
+    category = category_on_db[2]
 
-  client.headers = token
-  
-  body = {
-    "user_id": user.id,
-    "category_id": category.id,
-    "expense": {
-      "description": "Expense description",
-      "value": 99.99
+    client.headers = token
+
+    body = {
+        "user_id": user.id,
+        "category_id": category.id,
+        "expense": {
+            "description": "Expense description", 
+            "value": 99.99
+        },
     }
-  }
 
-  response = client.post('/expense/add', json=body)
+    response = client.post("/expense/add", json=body)
 
-  assert response.status_code == status.HTTP_201_CREATED
+    assert response.status_code == status.HTTP_201_CREATED
 
-  expenses_on_db = db_session.query(ExpenseModel).all()
+    expenses_on_db = db_session.query(ExpenseModel).all()
 
-  assert len(expenses_on_db) == 1
+    assert len(expenses_on_db) == 1
 
-  db_session.delete(expenses_on_db[0])
-  db_session.commit()
+    db_session.delete(expenses_on_db[0])
+    db_session.commit()
